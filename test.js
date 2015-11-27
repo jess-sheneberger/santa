cc4cb3ca5b1fcb2712121ed238e21ce81d207952
Jess Sheneberger <jess.sheneberger@neudesic.com>
2015-11-27 14:16:22 -0700

    fiddle with some stuff

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var run = require("./UnsecretSanta.js");
+var run = require(".");
 
 var santas = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ];
 
@@ -18,10 +18,16 @@ Object.keys(couples).forEach(function(z) {
 var history = {
 }
 
-it("should generate some results", function(done) {
+it("should generate some results after a few tries", function(done) {
     console.log("Run for test 1");
-    var assignments = run(santas, couples, history);
-    console.log("Results: "+ JSON.stringify(assignments, null, 4));
-    done();
-    console.log("Run for test 1 complete");
+    for (var i=0; i<5; i++) {
+        var assignments = run(santas, couples, history);
+        console.log("Results: "+ JSON.stringify(assignments, null, 4));
+        if (assignments) {
+            done();
+            console.log("Run for test 1 complete");
+            return;
+        }
+    }
+    throw new Error("no workie workie");
 });
